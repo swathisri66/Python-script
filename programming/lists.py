0cff06e57a6623a4c3884fedc4c8182e0b7c58be
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-24 19:08:05 +0200

    Create lists.py

---
+++ programming/lists.py
@@ -0,0 +1,23 @@
+#ordered set of values lists
+
+x = [3,6,9,5,9]
+print(x)
+print(len(x))
+print(x[4])
+
+
+x.insert(2, 'learn')
+
+print(x)
+x.pop()
+print(x)
+x.reverse()
+print(x)
+x.append(10)
+print(x)
+x.count(10)
+print(x)
+x.count(9)
+print(x)
+x.clear()
+print(x)

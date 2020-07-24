3b594ca77fdf1d27d079a79751cf99637cc1c56f
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-24 13:52:02 +0200

    Create ifelse.py

---
+++ programming/ifelse.py
@@ -0,0 +1,23 @@
+#if else operators
+x = -100
+
+if x != 100:
+    print("x is = ")
+    print(x)
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative") 
+print("finish")  
+
+#if else with boolean operations
+
+x = 100
+
+if x != 100 and x < 0:
+    print("x is = ")
+    print(x)
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative")

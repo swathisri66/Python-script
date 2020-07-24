4636a239e3c6e4cadabb7a8a2232107e81962732
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-24 19:48:14 +0200

    Create dictonary.py

---
+++ programming/dictonary.py
@@ -0,0 +1,26 @@
+# associative lists, maps, lists of pairs
+
+
+
+D = {'name': 'swathi', 'age': 36, 'year': 1984}
+print(D)
+print(D['name'])
+
+E = {'name': 'Kamani', 15: 34, True: True, (2,3): 9}
+print(E[(2,3)])
+
+print(len(E))
+print(D.get('name'))
+
+D.update({'name':'swathi'})
+print(D)
+D.keys()
+dict_keys(['name', 'year', 'age'])
+
+D.values()
+dict_values(['swathi','2000',36])
+
+D.items()
+print(D)
+dict_items([('name', 'max'), ('year', 2000), ('age', 36)])
+D.popitem()

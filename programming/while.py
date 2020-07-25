dff277686ff99aac085fa4de215437570eb19191
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-25 12:54:33 +0200

    Create while.py

---
+++ programming/while.py
@@ -0,0 +1,26 @@
+num = 1
+sum = 0
+print("Enter a number. Please enter zero(0) to exit")
+while num != 0:
+    num = float(input("Number ?"))
+    sum = sum +  num;
+    print("sum = ", sum)
+else:
+    print("Completed sum")
+   
+    
+i = 0
+
+while i < 5:
+    print("the value of i: ", i)
+    i += 1 
+else:
+    print("completed while loop")    
+    
+i = 0
+
+while True:
+    print("the value of i: ", i)
+    i += 1 
+else:
+    print("completed while loop")        

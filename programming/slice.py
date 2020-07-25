aa25ab84aa291f8fc06a48f99273e51966e18b2c
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-25 12:43:14 +0200

    Create slice.py

---
+++ programming/slice.py
@@ -0,0 +1,17 @@
+a = [0,1,2,3,4,5,6,7,8,9]
+b = (0,1,2,3,4,5,6,7,8,9)
+c = '0123456789'
+x =slice(0,5)
+print((a[x]))
+print(a[1:5])
+print(b[:6])
+print(b[4:])
+print(c[:])
+print(a[0:9:2])
+#print(a[0.9:3])
+print(a[: : 4])
+print([c[-2]])
+print(a[: : -1])
+print(a[: : 1])
+print(a[: -3:-1])
+print(a[-3: :-1])

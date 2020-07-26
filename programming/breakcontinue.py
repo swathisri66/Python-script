c3a5511e74e3a0a6091ec90164b59a39de1e4ce3
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-07-26 14:52:17 +0200

    Create breakcontinue.py

---
+++ programming/breakcontinue.py
@@ -0,0 +1,18 @@
+a = [0, 1, 2, 3, 4, 5]
+for x in a:
+    if x== 3:
+        #break
+        continue
+    print(x)
+    
+
+print('------------------------')
+   
+i = 0
+while i < 5:
+    if i == 4:
+        #break
+        continue        
+    print(i)
+    i += 1
+    

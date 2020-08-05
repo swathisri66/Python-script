6d63a29eb6b160439ff96e0a018effa6f3f2cecf
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-08-05 07:55:29 +0200

    Create datetime.py

---
+++ programming/datetime.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+import pytz
+from tkinter import *
+import time
+
+root=Tk()
+root.geometry("500*250")
+
+#def times():
+
+name=Label(root,text="swathi"font=("times",20,"bold"))
+name.1place(x=30,y=5)
+clock=Label(root,text="Kamani",font=("times",25,"bold"))
+clock.place(x=10,y=40)
+nota=Label(root,text="Hours minutes seconds",font="times 10  ")
+nota.place(x=10,y=80)
+
+name1=Label(root,text="swathi"font=("times",20,"bold"))
+name1.1place(x=30,y=5)
+clock1=Label(root,text="Kamani",font=("times",25,"bold"))
+clock1.place(x=10,y=40)
+nota1=Label(root,text="Hours minutes seconds",font="times 10  ")
+nota1.place(x=10,y=80)
+
+name2=Label(root,text="swathi"font=("times",20,"bold"))
+name2.1place(x=30,y=5)
+clock2=Label(root,text="Kamani",font=("times",25,"bold"))
+clock2.place(x=10,y=40)
+nota2=Label(root,text="Hours minutes seconds",font="times 10  ")
+nota2.place(x=10,y=80)
+
+name3=Label(root,text="swathi"font=("times",20,"bold"))
+name3.1place(x=30,y=5)
+clock3=Label(root,text="Kamani",font=("times",25,"bold"))
+clock3.place(x=10,y=40)
+nota3=Label(root,text="Hours minutes seconds",font="times 10  ")
+nota3.place(x=10,y=80)
+
+name4=Label(root,text="swathi"font=("times",20,"bold"))
+name4.1place(x=30,y=5)
+clock4=Label(root,text="Kamani",font=("times",25,"bold"))
+clock4.place(x=10,y=40)
+nota4=Label(root,text="Hours minutes seconds",font="times 10  ")
+nota4.place(x=10,y=80)
+
+root.mainloop()
+

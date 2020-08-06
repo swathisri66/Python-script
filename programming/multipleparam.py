bb74638dc4b1fc8f0790525ee4dd087430037aa6
swathisri66 <39615196+swathisri66@users.noreply.github.com>
2020-08-06 10:43:53 +0200

    Create multipleparam.py

---
+++ programming/multipleparam.py
@@ -0,0 +1,8 @@
+class Hello:
+    # def __init__(self): pass
+    #def __init__(self, name='max'): pass
+    def __init__(self, *args, **kwargs ): pass
+
+hello = Hello() name='sjdd')
+
+hello = Hello('name', 'ah', 'dd', name= 'sdd')
